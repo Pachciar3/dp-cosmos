d41acd3c3daf0776da95466ad0a662b46c5d9d1e
Pachciar <pachciar3@gmail.com>
2021-05-21 13:42:53 +0200

    Add sorting to Table

--- src/components/Table/TableHeadButton.js
+++ src/components/Table/TableHeadButton.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 import TwoArrowsIcon from '../../assets/TwoArrows';
+import DownArrowIcon from '../../assets/DownArrow';
+import UpArrowIcon from '../../assets/UpArrow';
 
-function TableHeadButton({ children, handleClick }) {
+function TableHeadButton({ children, handleClick, type }) {
+  function iconType() {
+    if (type === "DESC") {
+      return <DownArrowIcon color="#a094c6" />
+    } else if (type === "ASC") {
+      return <UpArrowIcon color="#a094c6" />
+    } else {
+      return <TwoArrowsIcon color="#a094c6" />
+    }
+  }
   return (
-    <th className="table__head"><button onClick={handleClick} className="table__head-button"><span>{children}</span><span className="table__head-button__icon"><TwoArrowsIcon color="#a094c6" /></span></button></th>
+    <th className="table__head">
+      <button onClick={handleClick} className="table__head-button">
+        <span>{children}</span>
+        <span className="table__head-button__icon">
+          {iconType()}
+        </span>
+      </button>
+    </th>
   )
 }
 
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 import { fetchRequested } from "./redux";
 
@@ -7,15 +7,25 @@ import TableLine from './TableLine';
 import TableCell from './TableCell';
 import './styles.scss';
 
+
+
 function Table({ fetchRequested, objects, columns, requestPath, isLoading, isError }) {
+  const SORT_TYPES = { [columns[0].value]: "none", [columns[1].value]: "none" }
+
+  const [data, setData] = useState(objects[requestPath]);
+  const [sortTypes, setSortTypes] = useState(SORT_TYPES);
   useEffect(() => {
     if (!objects[requestPath].length) {
       fetchRequested(requestPath);
+    } else {
+      setData(objects[requestPath])
     }
-  }, [])
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [objects])
   function renderList() {
-    if (objects) {
-      return objects[requestPath].map(object => (
+    if (data) {
+      return data.map(object => (
         <TableLine key={object.id}>
           <TableCell>{object[columns[0].value]}</TableCell>
           <TableCell>{object[columns[1].value]}</TableCell>
@@ -26,15 +36,49 @@ function Table({ fetchRequested, objects, columns, requestPath, isLoading, isErr
     return null
   }
   function handleSortButtonClick(name) {
-    console.log(name);
+    const type = sortTypes[name];
+    if (type === "DESC" || type === "none") {
+      const dataToSort = [...data]
+      dataToSort.sort((a, b) => {
+        let fa = a[name].toString().toLowerCase(),
+          fb = b[name].toString().toLowerCase();
+
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      })
+      setData(dataToSort);
+      setSortTypes({ ...SORT_TYPES, [name]: "ASC" });
+    } else if (type === "ASC") {
+      const dataToSort = [...data]
+      dataToSort.sort((a, b) => {
+        let fa = a[name].toString().toLowerCase(),
+          fb = b[name].toString().toLowerCase();
+
+        if (fa < fb) {
+          return 1;
+        }
+        if (fa > fb) {
+          return -1;
+        }
+        return 0;
+      })
+      setData(dataToSort);
+      setSortTypes({ ...SORT_TYPES, [name]: "DESC" });
+    }
+
   }
   return (
     <div>
       <table className="table">
         <thead>
           <TableLine>
-            <TableHeadButton handleClick={() => handleSortButtonClick(columns[0].value)}>{columns[0].name}</TableHeadButton>
-            <TableHeadButton handleClick={() => handleSortButtonClick(columns[1].value)}>{columns[1].name}</TableHeadButton>
+            <TableHeadButton type={sortTypes[columns[0].value]} handleClick={() => handleSortButtonClick(columns[0].value)}>{columns[0].name}</TableHeadButton>
+            <TableHeadButton type={sortTypes[columns[1].value]} handleClick={() => handleSortButtonClick(columns[1].value)}>{columns[1].name}</TableHeadButton>
           </TableLine>
         </thead>
         <tbody className="table__content">
