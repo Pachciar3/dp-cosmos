f75c7dc61a776416c88e8005946054f052b2017c
Pachciar <pachciar3@gmail.com>
2021-05-22 19:01:38 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga';
 
-import mySaga from './sagas'
+import mySaga from './sagas';
 
 import './index.scss';
 import './reset.css';
@@ -12,10 +12,10 @@ import App from './App';
 import rootReducer from './rootReducer';
 import reportWebVitals from './reportWebVitals';
 
-const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
-sagaMiddleware.run(mySaga)
+sagaMiddleware.run(mySaga);
 
 ReactDOM.render(
   <React.StrictMode>
